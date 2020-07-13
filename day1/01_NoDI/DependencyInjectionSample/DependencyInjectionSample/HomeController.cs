daf0d54f5203f05933fb1a6b751493a198d55a52
Christian Nagel <christian@christiannagel.com>
2020-07-13 11:57:46 +0200

    first DI sample

---
+++ day1/01_NoDI/DependencyInjectionSample/DependencyInjectionSample/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DependencyInjectionSample
+{
+    public class HomeController
+    {
+        public string Index(string name)
+        {
+            var service = new HelloService();
+            string result = service.Greet(name);
+            return result.ToUpper();
+        }
+    }
+}
---
+++ day1/01_NoDI/DependencyInjectionSample/DependencyInjectionSample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DependencyInjectionSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var controller = new HomeController();
+            string result = controller.Index("Stephanie");
+            Console.WriteLine(result);
+        }
+    }
+}
