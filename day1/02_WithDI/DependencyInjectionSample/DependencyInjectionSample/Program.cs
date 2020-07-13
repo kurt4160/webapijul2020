b9a8cce2bdc84e38fc41471ae6c16d435a1db3f1
Christian Nagel <christian@christiannagel.com>
2020-07-13 13:10:40 +0200

    with DI

---
+++ day1/02_WithDI/DependencyInjectionSample/DependencyInjectionSample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DependencyInjectionSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new HelloService();
+            var controller = new HomeController(service);
+            string result = controller.Index("Matthias");
+            Console.WriteLine(result);
+        }
+    }
+}
