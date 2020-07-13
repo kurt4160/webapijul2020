116c9fc0d63ea29d43e645bc872f6e80e93d744f
Christian Nagel <christian@christiannagel.com>
2020-07-13 14:50:16 +0200

    scopes added

--- day1/04_WithHost/DependencyInjectionSample/DependencyInjectionSample/Program.cs
+++ day1/04_WithHost/DependencyInjectionSample/DependencyInjectionSample/Program.cs
@@ -10,15 +10,16 @@ namespace DependencyInjectionSample
         static void Main(string[] args)
         {
             using var host = Host.CreateDefaultBuilder()
-                .ConfigureServices(services =>
-                {
-                    services.AddTransient<IHelloService, HelloService>();
-                    services.AddTransient<HomeController>();
-                })
-                .Build();
+                                .ConfigureServices(services =>
+                                {
+                                   
+                                    services.AddTransient<IHelloService, HelloService>();
+                                    services.AddTransient<HomeController>();
+                                }).Build();
+            using var scope1 = host.Services.CreateScope();
+            var controller = scope1.ServiceProvider.GetService<HomeController>();
 
-            var controller = host.Services.GetService<HomeController>();
-
+            // var controller = host.Services.GetService<HomeController>();
 
             string result = controller.Index("Katharina");
             Console.WriteLine(result);
