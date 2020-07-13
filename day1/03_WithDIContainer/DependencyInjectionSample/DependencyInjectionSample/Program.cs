dabd84ae915a34504850e6a3c6e782802f0b9a1b
Christian Nagel <christian@christiannagel.com>
2020-07-13 13:10:48 +0200

    DI container

---
+++ day1/03_WithDIContainer/DependencyInjectionSample/DependencyInjectionSample/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+
+namespace DependencyInjectionSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var container = GetServices();
+            var controller = container.GetService<HomeController>();
+
+            string result = controller.Index("Matthias");
+            Console.WriteLine(result);
+        }
+
+        static ServiceProvider GetServices()
+        {
+            var services = new ServiceCollection();
+            services.AddTransient<IHelloService, HelloService>();
+            services.AddTransient<HomeController>();
+            return services.BuildServiceProvider();
+        }
+    }
+}
