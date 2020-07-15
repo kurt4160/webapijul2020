39985c8f1bc77d7f36c372c11064b433e486718e
Christian Nagel <christian@christiannagel.com>
2020-07-15 17:13:13 +0200

    signalr service

---
+++ day3/MySignalRService/ClientApp/Program.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace ClientApp
+{
+    public class SomeData
+    {
+        public int Value { get; set; }
+
+        public override string ToString() => Value.ToString();
+    }
+
+    class Program
+    {
+        private static HubConnection? s_hubConnection;
+        static async Task Main()
+        {
+            Console.WriteLine("client - wait for service...");
+            Console.ReadLine();
+
+            await ServerToClientStreamingAsync();
+            // await ClientToServerStreamingAsync();
+        }
+
+        private static async Task ServerToClientStreamingAsync()
+        {
+            s_hubConnection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:5001/myhub")
+                .Build();
+
+            s_hubConnection.Closed += async (ex) =>
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("restart");
+                await Task.Delay(new Random().Next(0, 5) * 1000);
+                await s_hubConnection.StartAsync();
+            };
+
+            await s_hubConnection.StartAsync();
+
+            // read from the hub using async streams
+            var stream = s_hubConnection.StreamAsync<SomeData>("GetSomeDataWithAsyncStreams", 20, 1000);
+            await foreach (var d in stream)
+            {
+                Console.WriteLine($"received {d}");
+            }
+
+            // https://github.com/aspnet/AspNetCore.Docs/blob/master/aspnetcore/signalr/streaming.md
+        }
+    }
+}
---
+++ day3/MySignalRService/MySignalRService/Hubs/MyHub.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySignalRService.Hubs
+{
+    public class SomeData
+    {
+        public int Value { get; set; }
+
+        public override string ToString() => Value.ToString();
+    }
+
+    public class MyHub : Hub
+    {
+        public override Task OnConnectedAsync()
+        {
+            return base.OnConnectedAsync();
+        }
+
+        public async Task SomeMessage(string user, string message)
+        {
+            await base.Clients.All.SendAsync("Broadcast", user, message);
+        }
+
+        public async IAsyncEnumerable<SomeData> GetSomeDataWithAsyncStreams(int count, int delay)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                await Task.Delay(delay);
+                yield return new SomeData { Value = i };
+            }
+        }
+    }
+}
