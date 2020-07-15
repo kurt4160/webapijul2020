e400630e1bd614b140ab984739fe4f8227281832
Christian Nagel <christian@christiannagel.com>
2020-07-15 13:09:59 +0200

    web app with local users

---
+++ day3/WebAppWithLocalUsers/WebAppWithLocalUsers/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebAppWithLocalUsers.Models;
+
+namespace WebAppWithLocalUsers.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize(Roles ="Developers")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        //public IActionResult SomeData(int x)
+        //{
+        //    User.IsInRole()
+        //}
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
