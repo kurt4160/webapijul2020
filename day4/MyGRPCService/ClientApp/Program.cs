5241557ea4afba7a4521ed41f2f231f16b440138
Christian Nagel <christian@christiannagel.com>
2020-07-16 10:54:26 +0200

    grpc service

---
+++ day4/MyGRPCService/ClientApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using Grpc.Net.Client;
+using MyGRPCService;
+using System;
+using System.Threading.Tasks;
+
+namespace ClientApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            Greeter.GreeterClient client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = "Matthias" });
+            Console.WriteLine(reply.Message);
+        }
+    }
+}
