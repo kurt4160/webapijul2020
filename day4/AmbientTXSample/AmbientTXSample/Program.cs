5ab74f88982aa5759f0c96c8864359e8e97c1c4a
Christian Nagel <christian@christiannagel.com>
2020-07-16 15:49:46 +0200

    ambient tx

---
+++ day4/AmbientTXSample/AmbientTXSample/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Transactions;
+
+namespace AmbientTXSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var scope = new TransactionScope(TransactionScopeOption.Required))
+            {
+                Transaction.Current.TransactionCompleted += (sender, e) =>
+                {
+                    ShowTx("completed", e.Transaction.TransactionInformation);
+                };
+
+                ShowTx("in tx", Transaction.Current.TransactionInformation);
+
+             //   scope.Complete();
+            }  // commit - rollback
+
+            
+        }
+
+        static void ShowTx(string status, TransactionInformation info)
+        {
+            Console.WriteLine(status);
+            Console.WriteLine(info.Status);
+            Console.WriteLine(info.LocalIdentifier);
+            Console.WriteLine(info.DistributedIdentifier);
+
+        }
+    }
+}
